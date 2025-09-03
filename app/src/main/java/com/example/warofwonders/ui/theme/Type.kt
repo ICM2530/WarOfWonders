0745a243e100d6dfbd445321150d54e72bb86ace
Dani2044 <castro-df@javeriana.edu.co>
2025-09-02 22:06:16 -0500

    Typography

--- app/src/main/java/com/example/warofwonders/ui/theme/Type.kt
+++ app/src/main/java/com/example/warofwonders/ui/theme/Type.kt
@@ -2,33 +2,39 @@ package com.example.warofwonders.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.warofwonders.R
 
-// Set of Material typography styles to start with
+val PixelifySansFamily = FontFamily(
+    Font(R.font.pixelifysans_bold, FontWeight.Bold),
+    Font(R.font.pixelifysans_medium, FontWeight.Medium),
+    Font(R.font.pixelifysans_regular, FontWeight.Normal),
+    Font(R.font.pixelifysans_semibold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = PixelifySansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = PixelifySansFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = PixelifySansFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
